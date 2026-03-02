00006807fcd96565bc4c1a2deddcab59758b17ee
sergious234 <sergious234@gmail.com>
2026-03-02 01:40:25 +0100

    remove deadcode and clangd

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,7 +12,8 @@ vim.opt.undofile = true
 vim.opt.signcolumn = "yes"
 vim.opt.foldmethod = "indent"
 vim.lsp.inlay_hint.enable(true)
-
+vim.lsp.buf.format({async = true})
+vim.diagnostic.config({ virtual_text = true })
 
 require "colorizer".setup()
 require "mason".setup()
@@ -41,12 +42,12 @@ vim.lsp.config("tsserver", {
 
 vim.lsp.config("clangd", {
 	cmd = {
-    "clangd",
-    "--background-index",
-    "--clang-tidy",
-    "--header-insertion=never",
-  },
-	filetypes = { "c", "cpp", "objc", "objcpp" },
+		"clangd",
+		"--background-index",
+		"--clang-tidy",
+		"--header-insertion=never",
+	},
+	filetypes = { "c", "cpp", "objc", "objcpp", "h", "hpp"},
 
 	root_markers = {
 		'.clang-tidy'
@@ -61,8 +62,8 @@ vim.lsp.config("clangd", {
 	},
 	init_options = {
 		clangdFileStatus = true,
-    completeUnimported = true,
-		fallbackFlags = { '--std=c++23', '-Wall', '-Wextra'}
+		completeUnimported = true,
+		fallbackFlags = { '--std=c++23', '-Wall', '-Wextra' }
 	},
 })
 
@@ -73,58 +74,8 @@ require('nvim-treesitter.configs').setup({
 	ensure_installed = { "c", "lua", "rust", "typescript", "python", "java", "cpp", "regex" },
 })
 
-
-math.randomseed(os.time())
-function Random_theme()
-	local schemes = {
-		{
-			lualine = "gruvbox-material",
-			colorscheme = "gruvbox-material",
-		},
-
-		{
-			lualine = "dracula",
-			colorscheme = "tokyonight-storm",
-		},
-		{
-			lualine = "everforest",
-			colorscheme = "everforest",
-		},
-
-		{
-			lualine = "ayu_mirage",
-			colorscheme = "catppuccin-mocha",
-		},
-		{
-			lualine = "vague",
-			colorscheme = "vague",
-		}
-	}
-
-	local i = math.random(#schemes)
-	return schemes[i]
-end
-
--- Reload theme
-function Reload_theme()
-	local theme = Random_theme().colorscheme
-	vim.cmd("colorscheme " .. theme)
-	vim.notify("New theme: " .. "[" .. theme .. "]" .. " !", vim.log.levels.INFO)
-end
-
-Theme = Random_theme()
 vim.cmd("colorscheme " .. "vague")
 
-vim.diagnostic.config({ virtual_text = true })
--- local _general = vim.api.nvim_create_augroup("_general", { clear = true })
--- vim.api.nvim_create_autocmd(
--- 	{ "BufEnter", "InsertLeave" },
--- 	{
--- 		pattern = "*.rs",
--- 		command = "lua vim.lsp.codelens.refresh({ bufnr = 0 })",
--- 		group = _general
--- 	}
--- )
 
 vim.g.rustaceanvim = {
 	-- Plugin configuration
@@ -142,6 +93,9 @@ vim.g.rustaceanvim = {
 			vim.keymap.set("n", "grm", function() vim.cmd.RustLsp('openDocs') end, { desc = "Open docs.rs" })
 			vim.keymap.set("n", "gre", function() vim.cmd.RustLsp('explainError') end, { desc = "Explain error >.<" }) -- default to 'cycle'
 
+			vim.keymap.set("n", "grc", function() vim.cmd.RustLsp('flyCheck', 'run') end, { desc = "check" })
+
+			vim.keymap.set("n", "grr", function() vim.cmd.RustLsp('run') end, { desc = "run" })
 			-- you can also put keymaps in here
 			vim.keymap.set("n", "K", function() vim.cmd.RustLsp({ 'hover', 'actions' }) end,
 				{ silent = true, buffer = bufnr, desc = "Show info" })
@@ -150,6 +104,10 @@ vim.g.rustaceanvim = {
 		default_settings = {
 			-- rust-analyzer language server configuration
 			["rust-analyzer"] = {
+				checkOnSave = false,
+				procMacro = {
+					enable = true,
+				},
 				inlayHints = {
 					enable = true,
 					renderColons = true,
@@ -159,6 +117,7 @@ vim.g.rustaceanvim = {
 					typeHints = {
 						enable = true,
 					},
+					chainingHints = { enable = true }
 				},
 				check = {
 					command = "clippy",
@@ -178,68 +137,3 @@ vim.g.rustaceanvim = {
 	-- DAP configuration
 	dap = {},
 }
-
-
--- function _G.get_oil_winbar()
--- 	local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
--- 	local dir = require("oil").get_current_dir(bufnr)
--- 	if dir then
--- 		return vim.fn.fnamemodify(dir, ":~")
--- 	else
--- 		-- If there is no current directory (e.g. over ssh), just show the buffer name
--- 		return vim.api.nvim_buf_get_name(0)
--- 	end
--- end
-
---[[
-require "oil".setup({
-	experimental_watch_for_changes = true,
-	view_options = {
-		is_always_hidden = function(name, _)
-			return name == "target" or name == ".git"
-		end
-	},
-
-	buf_options = {
-		buflisted = false,
-		bufhidden = "hide",
-	},
-
-	columns = {
-		"icon",
-		-- "permissions",
-		"size",
-		-- "mtime",
-	},
-	preview_win = {
-		-- Whether the preview window is automatically updated when the cursor is moved
-		update_on_cursor_moved = true,
-		-- How to open the preview window "load"|"scratch"|"fast_scratch"
-		preview_method = "fast_scratch",
-		-- A function that returns true to disable preview on a file e.g. to avoid lag
-		disable_preview = function(filename)
-			return false
-		end,
-		-- Window-local options to use for preview window buffers
-		win_options = {},
-	},
-	keymaps = {
-		["g?"] = { "actions.show_help", mode = "n" },
-		["<CR>"] = "actions.select",
-		["<C-s>"] = { "actions.select", opts = { vertical = true } },
-		["<C-h>"] = { "actions.select", opts = { horizontal = true } },
-		["<C-t>"] = { "actions.select", opts = { tab = true } },
-		["<C-p>"] = "actions.preview",
-		["<C-c>"] = { "actions.close", mode = "n" },
-		["<C-l>"] = "actions.refresh",
-		["-"] = { "actions.parent", mode = "n" },
-		["_"] = { "actions.open_cwd", mode = "n" },
-		["`"] = { "actions.cd", mode = "n" },
-		["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
-		["gs"] = { "actions.change_sort", mode = "n" },
-		["gx"] = "actions.open_external",
-		["g."] = { "actions.toggle_hidden", mode = "n" },
-		["g\\"] = { "actions.toggle_trash", mode = "n" },
-	},
-})
-]] --
